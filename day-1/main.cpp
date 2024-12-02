11962ade4c64b1f89051ad959b0af73d569cccb1
Karol Orszulik <orkarox@gmail.com>
2024-12-02 21:31:29 +0100

    Day 1 part 2

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -34,10 +34,8 @@ Lists loadLists(const char* path)
 	return lists;
 }
 
-int main()
+int part1(Lists const& lists)
 {
-	Lists lists = loadLists("input.txt");
-	
 	int sum = 0;
 
 	for (int i = 0; i < lists.l1.size(); i++)
@@ -45,6 +43,25 @@ int main()
 		sum += std::abs(lists.l1[i] - lists.l2[i]);
 	}
 
-	std::cout << sum << std::endl;	
+	return sum;
+}
 
+int part2(Lists const& lists)
+{
+	int score = 0;
+
+	for (int el : lists.l1)
+	{
+		score += el * std::count(lists.l2.begin(), lists.l2.end(), el);
+	}
+
+	return score;
+}
+
+int main()
+{
+	Lists lists = loadLists("input.txt");
+	
+	std::cout << part1(lists) << std::endl;
+	std::cout << part2(lists) << std::endl;	
 }
