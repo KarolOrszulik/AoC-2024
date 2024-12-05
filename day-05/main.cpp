ff95f2844866eb2ed9f65de07a62c8cad61e1e23
KarolOrszulik <orkarox@gmail.com>
2024-12-05 15:17:42 +0100

    Day 5

--- day-05/main.cpp
+++ day-05/main.cpp
@@ -3,6 +3,9 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <regex>
 
 class Solution
 {
@@ -11,13 +14,11 @@ public:
     {
         std::ifstream file(path);
 
-        if (file)
+        if (!file)
         {
             std::cerr << "Could not open input file" << std::endl;
             return;
         }
-
-        std::cout << file.rdbuf();
 
         std::string line;
 
@@ -33,8 +34,34 @@ public:
         }
     }
 
+    int part1()
+    {
+        int sum = 0;
+        for (auto const& update : updates)
+        {
+            if (isUpdateCorrect(update))
+            {
+                sum += middlePage(update);
+            }
+        }
+        return sum;
+    }
+
+    int part2()
+    {
+        int sum = 0;
+        for (auto const& update : updates)
+        {
+            if (isUpdateCorrect(update))
+                continue;
+            
+            sum += middlePage(fixUpdate(update));
+        }
+        return sum;
+    }
+
 private:
-    using OrderingRule = std::pair<int, int>;
+    using OrderingRules = std::unordered_map<int, std::unordered_set<int>>;
     using Update = std::vector<int>;
 
     void parseOrderingRule(std::string const& rule)
@@ -45,31 +72,75 @@ private:
         std::istringstream iss(rule);
         iss >> early >> pipe >> late;
 
-        rules.emplace_back(early, late);
+        rules[early].insert(late);
     }
 
     void parseUpdate(std::string const& numbers)
     {
         Update result;
 
-        std::istringstream iss(numbers);
-        int n;
-        while (iss >> n)
+        std::regex numberRegex("\\d+");
+        for (std::sregex_iterator it(numbers.begin(), numbers.end(), numberRegex); it != std::sregex_iterator(); ++it)
         {
-            result.push_back(n);
+            result.push_back(std::stoi(it->str()));
         }
 
         updates.push_back(result);
     }
 
-    std::vector<OrderingRule> rules;
+    bool isUpdateCorrect(Update const& update)
+    {
+        for (int i = 0; i < update.size() - 1; i++)
+        {
+            const int current = update[i];
+            for (int j = i + 1; j < update.size(); j++)
+            {
+                const int second = update[j];
+                if (rules[current].count(second) == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    void fixSingleError(Update& update)
+    {
+        for (int i = 0; i < update.size() - 1; i++)
+        {
+            const int current = update[i];
+            for (int j = i + 1; j < update.size(); j++)
+            {
+                const int second = update[j];
+                if (rules[current].count(second) == 0)
+                    std::swap(update[i], update[j]);
+            }
+        }
+    }
+
+    Update fixUpdate(Update const& update)
+    {
+        Update fixed = update;
+        while (!isUpdateCorrect(fixed))
+        {
+            fixSingleError(fixed);
+        }
+        return fixed;
+    }
+
+    int middlePage(Update const& update)
+    {
+        return update[update.size() / 2];
+    }
+
+    OrderingRules rules;
     std::vector<Update> updates;
 };
 
 int main()
 {
-    system("pwd");
-    system("ls ../../");
     Solution solution;
-    solution.loadInput("../../input.txt");
+    
+    solution.loadInput("../input.txt");
+    std::cout << solution.part1() << std::endl;
+    std::cout << solution.part2() << std::endl;
 }
